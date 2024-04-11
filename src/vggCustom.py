47ed8914c5950b708677726b5e4dd8fd56a72ca9
Shayaan Khan <shayaanmk@gmail.com>
2024-04-11 13:56:34 -0400

    Added vggCustom.py, implementation according to the literature

---
+++ src/vggCustom.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+# This Custom VGG model follows the VGG-16 architecture with convolutional dropout implementation
+class VGGCustom(nn.Module):
+    def __init__(self):
+        super(VGGCustom, self).__init__()
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn6 = nn.BatchNorm2d(128)
+        self.conv7 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn7 = nn.BatchNorm2d(256)
+        self.conv8 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.bn8 = nn.BatchNorm2d(128)
+        self.conv9 = nn.Conv2d(128, 1024, kernel_size=3, padding=1)
+        self.bn9 = nn.BatchNorm2d(1024)
+        self.conv10 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.bn10 = nn.BatchNorm2d(512)
+        # Add dropout here
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(4608, 512)
+        self.fc2 = nn.Linear(512, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.fc4 = nn.Linear(512, 512)
+        self.fc5 = nn.Linear(512, 256)
+        self.fc6 = nn.Linear(256, 64)
+        self.predictions = nn.Linear(64, 2)
+
+        # Pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Dropout layer
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = F.relu(self.bn8(self.conv8(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn9(self.conv9(x)))
+        x = F.relu(self.bn10(self.conv10(x)))
+        x = self.dropout(x)
+        x = self.pool(x)
+
+        # Flatten the output for FC layers
+        x = x.view(-1, 4608)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc6(x))
+        x = self.predictions(x)
+
+        # Apply softmax for prediction probabilities
+        x = F.softmax(x)
+        return x
